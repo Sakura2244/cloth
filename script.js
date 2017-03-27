53dd5f525d9b512d1a3a145d6bf861441a49f0d1
Jennifer <jenniferzheng23@gmail.com>
2017-03-27 21:15:12 +0000

    made the button work

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 // JavaScript File
- $("button").click(function(){
-     var word = "shirt";
+
+ $("#button1").click(function(){
        var input = $("#item").val();
-       if(input === word){
-            $("#result").append(price_check());
+       if(input === "shirt"){
+            $("#result").html(("$65USD"));
+            $("#result").css("color","blue");
+       } else if(input ==="pant"){
+            $("#result").html(("$103USD"));
+            $("#result").css("color","blue");
+       } else if(input==="scarf"){
+           $("#result").html(("$31USD"));
             $("#result").css("color","blue");
        }
-});
-function price_check(){
-    return 65;
-}
-      
+ });
+ function convert(x){
+         return(x*1.09);
+     }
+ $("#button2").click(function(){
+     $("#result").html(convert($("#item").val()));
+ });
